daed65d8e851a301a74b573b35f0dc1dc5031bad
kuromiichi <afbarranco@gmail.com>
2023-05-23 11:10:56 +0200

    Refactorización AppConfig y DatabaseManager

--- Programacion/ProyectoFinalITV/src/main/kotlin/dev/team/proyectofinalitv/config/AppConfig.kt
+++ Programacion/ProyectoFinalITV/src/main/kotlin/dev/team/proyectofinalitv/config/AppConfig.kt
@@ -1,13 +1,16 @@
 package dev.team.proyectofinalitv.config
 
+import mu.KotlinLogging
 import java.io.File
 import java.io.FileInputStream
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.*
 
 
 class AppConfig {
 
-    private val logger = mu.KotlinLogging.logger {}
+    private val logger = KotlinLogging.logger {}
 
     // Constantes de ruta
     private val CONFIG_FILE = "application.properties"
@@ -16,55 +19,61 @@ class AppConfig {
     private val RESOURCE_PATH_MAIN =
         "$APP_PATH${File.separator}src${File.separator}main${File.separator}resources${File.separator}"
 
-    private var _urlConnectionSb: String = "" // URL donde nos conectaremos a la base de datos
-    fun getUrlConnectionDb(): String {
-        return _urlConnectionSb
-    }
+    // Propiedades
+    private lateinit var _urlConnection: String
+    val urlConnection get() = _urlConnection
 
-    private var _nameDb: String = ""
-    fun getNameDb(): String {
-        return _nameDb
-    }
+    private lateinit var _dbName: String
+    val dbName get() = _dbName
 
-    private var _passDb: String = ""
-    fun getPassDb(): String {
-        return _passDb
-    }
+    private lateinit var _dbUser: String
+    val dbUser get() = _dbUser
 
-    private var _userDb: String = ""
-    fun getUserDb(): String {
-        return _userDb
-    }
+    private lateinit var _dbPassword: String
+    val dbPassword get() = _dbPassword
 
-    private var _defaultDb: Boolean = false // Si permitimos reiniciar por defecto la base de datos
-    fun getDefaultDb(): Boolean {
-        return _defaultDb
-    }
+    private var _resetDb: Boolean = false
+    val resetDb get() = _resetDb
+
+    private lateinit var _dataRoot: String
+    val dataRoot get() = _dataRoot
+
+    private lateinit var _dataInput: String
+    val dataInput get() = _dataInput
+
+    private lateinit var _dataOutput: String
+    val dataOutput get() = _dataOutput
 
     init {
         loadProperties()
+        initStorage()
     }
 
     // Cargamos siempre en primer lugar el fichero properties
     private fun loadProperties() {
         logger.debug { "Cargando properties (configuración)" }
 
-        _urlConnectionSb = readProperty("db.url.connection", "jdbc:mariadb://localhost:3306")
-        _nameDb = readProperty("db.name", "ItvDam")
-        _userDb = readProperty("db.user", "root")
-        _passDb = readProperty("db.pass", "1234")
-        _defaultDb = readProperty("db.default", "false").toBoolean()
+        val props = Properties()
+        props.load(AppConfig::class.java.getResourceAsStream(CONFIG_FILE))
+
+        // Database
+        _urlConnection = props.getProperty("db.url.connection", "jdbc:mariadb://localhost:3306")
+        _dbName = props.getProperty("db.name", "ItvDam")
+        _dbUser = props.getProperty("db.user", "root")
+        _dbPassword = props.getProperty("db.pass", "1234")
+        _resetDb = props.getProperty("db.default", "false").toBoolean()
+
+        // Directorios
+        _dataRoot = props.getProperty("data.root", "data")
+        _dataInput = dataRoot + File.separator + props.getProperty("data.input", "input")
+        _dataOutput = dataRoot + File.separator + props.getProperty("data.output", "output")
     }
 
-    private fun readProperty(propertie: String, defaultValuePropertie: String): String {
-        logger.debug { "Leyendo propiedad: $propertie" }
+    private fun initStorage() {
+        logger.debug { "Inicializando storage (configuración)" }
 
-        val properties = Properties()
-        // Accedemos al fichero de propiedades mediante su nombre
-        val propertiesFile = ClassLoader.getSystemResource(CONFIG_FILE).file
-        // Cargamos el fichero a la clase Properties
-        properties.load(FileInputStream(propertiesFile))
-
-        return properties.getProperty(propertie, defaultValuePropertie)
+        // Creamos los directorios necesarios para el intercambio de datos
+        Files.createDirectories(Paths.get(dataInput))
+        Files.createDirectories(Paths.get(dataOutput))
     }
 }
--- Programacion/ProyectoFinalITV/src/main/kotlin/dev/team/proyectofinalitv/services/database/DataBaseManager.kt
+++ Programacion/ProyectoFinalITV/src/main/kotlin/dev/team/proyectofinalitv/services/database/DataBaseManager.kt
@@ -1,13 +1,11 @@
 package dev.team.proyectofinalitv.services.database
 
+import java.sql.Connection
 import java.sql.PreparedStatement
 import java.sql.Statement
 
 interface DataBaseManager {
-    fun openConnection()
-    fun closeConnection()
-    fun selectDataBase()
-    fun createStatement(): Statement?
-    fun prepareStatement(sql:String): PreparedStatement?
-    fun prepareStatementReturnGeneratedKey(sql: String): PreparedStatement?
+    fun createDatabase()
+    fun dropDatabase()
+    fun Connection.selectDatabase()
 }
